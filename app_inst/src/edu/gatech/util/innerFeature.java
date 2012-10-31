6851014830dbd0d38506a933dafc56f7f05f2ef7
xiaoyangmie <juyuanyang@gmail.com>
2012-10-31 15:26:22 -0300

    Update app_inst/src/edu/gatech/util/innerFeature.java

--- app_inst/src/edu/gatech/util/innerFeature.java
+++ app_inst/src/edu/gatech/util/innerFeature.java
@@ -9,12 +9,34 @@ import android.util.Log;
 public class innerFeature {
 	static int counter = 0;
 
-	public static Map<String, Integer> allFeatures = new HashMap<String, Integer>();
+	private static Map<String, String> featureKeySet = new HashMap<String, String>();
+	public static Map<String, Integer> featureValSet = new HashMap<String, Integer>();
 
-	public static void addFeature(String name, int value) {
-		System.out.println("AddFeature " + name + " " + value);
-		Log.e("Profile:  ", "AddFeature " + name + " " + value);
-		allFeatures.put(name, value);
+	public static void addFeature(String tag, String name, int value) {
+		System.out.println("Add feature: " + tag +" " + name + " " + value);
+		if(!featureKeySet.containsKey(tag)) {
+			featureKeySet.put(tag, name);
+			featureValSet.put(name, value);
+			System.out.println(tag + " " + name + " " + value);
+		}
+		else {
+			int newVal = featureValSet.get(name);
+			if (value > 0) newVal++;
+			featureValSet.put(name, newVal);
+			System.out.println(tag + " " + name + " " + newVal);
+		}
+	}
+
+	public static String getName(String tag) {
+		if (featureKeySet.containsKey(tag)) {
+			return featureKeySet.get(tag);
+		}
+		else {
+			String name = "c" + (counter++);
+			featureKeySet.put(tag, name);
+			featureValSet.put(name, 0);
+			return name;
+		}
 	}
 
 	public static String getNextName() {
