f1e8e7f2f12821c635bd89fd776bf984ccdc9ae6
vincent <vincent.nkq@gmail.com>
2012-11-07 23:54:44 -0500

    modify foba_poly.m in the inner ml4profiler directory

--- app_inst/ml4profiler/ml/stable/foba_poly_model.m
+++ app_inst/ml4profiler/ml/stable/foba_poly_model.m
@@ -3,7 +3,7 @@
 %% Parsing argument list
 arg_list = argv ();
 for i = 1:nargin
-%    printf ('%s\n', arg_list{i});
+    printf ('%s\n', arg_list{i});
 end
 dir_input = arg_list{1};
 key = '';
@@ -32,34 +32,81 @@ load(file_data);
 load(file_var);
 
 costly_f = load(file_costly);
-%disp(costly_f);
+printf("costly_f = load(file_costly): \n");
+disp(costly_f);
+printf("num_orig_feats: \n");
 disp(num_orig_feats);
 raw_data = zeros(length(runtime), num_orig_feats);
+printf("raw_data = zeros(length(runtime), num_orig_feats): \n");
 disp(raw_data);
 raw_data(:, var_f) = var_data;
-disp(raw_data);
+printf("raw_data(:, var_f) = var_data: \n");
+disp(raw_data(:, var_f));
 
 useful_f = setdiff(var_f, costly_f);
+printf("useful_f = setdiff(var_f, costly_f): \n");
+disp(useful_f);
 features = (raw_data(:, useful_f));
+printf("features = (raw_data(:, useful_f)): \n");
+disp(features);
 [num_data, D] = size(features);
+printf("[num_data, D] = size(features): \n");
+disp([num_data, D]);
 costs = ones(1, D);
+printf("costs = ones(1, D): \n");
+disp(costs);
 
 %% Randomly split data into training and testing sets
 rand_indics = 1:num_data;
+printf("rand_indics = 1:num_data: \n");
+disp(rand_indics);
 num_train = floor(portion*num_data);
+printf("num_train = floor(portion*num_data): \n");
+disp(num_train);
 train_indics = rand_indics(1:num_train);
+printf("train_indics = rand_indics(1:num_train): \n");
+disp(train_indics);
 test_indics  = rand_indics(num_train+1:num_data);
+printf("test_indics  = rand_indics(num_train+1:num_data): \n");
+disp(test_indics);
 y      = runtime(train_indics);
+printf("y      = runtime(train_indics): \n");
+disp(y);
 data   = features(train_indics, :);
+printf("data   = features(train_indics, :): \n");
+disp(data);
 y_test    = runtime(test_indics, 1);
+printf("y_test    = runtime(test_indics, 1): \n");
+disp(y_test);
 data_test = features(test_indics, :);
+printf("data_test = features(test_indics, :): \n");
+disp(data_test);
 
 %% Foba polynomial regression
 [err_sp_nl, num_chosen_feats, num_chose_terms, x_sp_nl, chosen_seqs, y_predict] = ...
    foba_poly_fitting_testing(y, data, y_test, data_test, costs, degree, threshold, nu, f_scale, max_terms, 0);
+printf("calling [err_sp_nl, num_chosen_feats, num_chose_terms, x_sp_nl, chosen_seqs, y_predict] = foba_poly_fitting_testing(y, data, y_test, data_test, costs, degree, threshold, nu, f_scale, max_terms, 0): \n");
+printf("err_sp_nl: \n");
+disp(err_sp_nl);
+printf("num_chosen_feats: \n");
+disp(num_chosen_feats);
+printf("num_chose_terms: \n");
+disp(num_chose_terms);
+printf("x_sp_nl: \n");
+disp(x_sp_nl);
+printf("chosen_seqs: \n");
+disp(chosen_seqs);
+printf("y_predict: \n");
+disp(y_predict);
 non0 = find(sum(chosen_seqs)>0);
+printf("non0 = find(sum(chosen_seqs)>0): \n");
+disp(non0);
 chosen_feats = useful_f(non0(2:end)-1);
+printf("chosen_feats = useful_f(non0(2:end)-1): \n");
+disp(chosen_feats);
 poly_terms = sequence2term(chosen_seqs, useful_f, 1);
+printf("poly_terms = sequence2term(chosen_seqs, useful_f, 1): \n");
+disp(poly_terms);
 
 %% output information
 %% The model to screen:
--- app_inst/ml4profiler/ml/stable/foba_poly_model_init.m
+++ app_inst/ml4profiler/ml/stable/foba_poly_model_init.m
@@ -2,9 +2,9 @@
 
 %% Parsing argument list
 arg_list = argv ();
-%for i = 1:nargin
-%    printf ('%s\n', arg_list{i});
-%end
+for i = 1:nargin
+    printf ('%s\n', arg_list{i});
+end
 dir_input = arg_list{1};
 key = '';
 runs = str2num(arg_list{3});
@@ -17,44 +17,66 @@ file_costly = strcat(dir_input, key, '/costly_features.txt');
 
 %% Reading data
 % Execution time file
+% Format: raw_time
+%     r1  r2  r3
+% m1
+% m2
+% m3
 file_exectime = strcat(dir_input, key, '/exectime.txt');
 raw_time = load(file_exectime);
-%disp(raw_time);
+printf("raw_time = load(file_exectime): \n");
+disp(raw_time);
 % 10
 % 5
 % 15
 runtime = sum(raw_time, 2)/runs;
-%disp(runtime);      <-------------- note: the only uncertain thing in this file is the runtime format. I can make it run successfully, but cannot understand..
+printf("runtime = sum(raw_time, 2)/runs: \n");
+disp(runtime);
 % 3.333
 % 1.666
 % 5.000
 
 % Cost file
+% Format: costs after being transposed
+% c1 c2 c3 c4
 file_feature_cost = strcat(dir_input, key, '/feature_cost.txt');
 costs = load(file_feature_cost);
 costs = costs';
-%disp(costs);
+printf("costs = load(file_feature_cost)': \n"); 
+disp(costs);
 % 1 1 1 1
 
 % Feature file
+% Format: raw_data after being transposed
+%    f1  f2  f3  f4
+% m1
+% m2
+% m3
 file_feature_data = strcat(dir_input, key, '/feature_data.txt');
 raw_data = load(file_feature_data);
 raw_data = raw_data';
 num_orig_feats = size(raw_data, 2);
-%disp(raw_data);
+printf("raw_data = load(file_feature_data)': \n");
+disp(raw_data);
 % 10 1 1 1
 % 1 10 1 1
 % 1 1 10 1
-%disp(num_orig_feats);
+printf("num_orig_feats = size(raw_data, 2): \n");
+disp(num_orig_feats);
 % 4
 
 %% Intermediate files
 % Get features with variability
+% Format: var_data
+%  f1  f2  f3  f4  column-wise
 var_data = var(raw_data);
-%disp(var_data);
+printf("var_data = var(raw_data): \n");
+disp(var_data);
 % 27 27 27 0
+% find non-zero columns
 var_f = find(var_data > 0);
-%disp(var_f);
+printf("var_f = find(var_data > 0): \n");
+disp(var_f);
 % 1 2 3
 %disp(raw_data(:, var_f));
 % 10 1 1
@@ -62,38 +84,52 @@ var_f = find(var_data > 0);
 % 1 1 10
 %disp(costs(var_f));
 % 1 1 1
-% Remove redudant features
+% Remove redundant features
 [org_data, unique_f] = remove_identical_cols(raw_data(:, var_f), costs(var_f));
-%disp(org_data);
+printf("call [org_data, unique_f] = remove_identical_cols(raw_data(:, var_f), costs(var_f)): \n");
+printf("matrix raw_data(:, var_f): \n");
+disp(raw_data(:, var_f));
+printf("matrix costs(var_f): \n");
+disp(costs(var_f));
+printf("matrix unique_f: \n");
+disp(unique_f);
+printf("matrix org_data: \n");
+disp(org_data);
 % 1 0 0
 % 0 1 0
 % 0 0 1
 %disp(unique_f);
 % 1 2 3
 var_f = var_f(unique_f);
-%disp(var_f);
+printf("var_f = var_f(unique_f): \n");
+disp(var_f);
 % 1 2 3
 
 raw_data = normalization(raw_data);
-%disp(raw_data);
+printf("raw_data = normalization(raw_data): \n");
+disp(raw_data);
 % 1 0 0 1
 % 0 1 0 1
 % 0 0 1 1
 costly_f = [0];
 
 [num_data, D] = size(raw_data);
-%disp([num_data, D]);
+printf("[num_data, D] = size(raw_data)): \n");
+disp([num_data, D]);
 % 3 4
 rand_indics = randperm(num_data);
-%disp(rand_indics);      <------ note: this is a random array
+printf("rand_indics = randperm(num_data): \n");
+disp(rand_indics);
 % 3 2 1
 var_data = raw_data(rand_indics, var_f);
-%disp(var_data);
+printf("var_data = raw_data(rand_indics, var_f): \n");
+disp(var_data);
 % 0 1 0
 % 1 0 0
 % 0 0 1
 runtime = runtime(rand_indics);
-%disp(runtime);
+printf("runtime = runtime(rand_indics): \n");
+disp(runtime);
 % 5.000
 % 1.666
 % 3.333
