f5d15e3f5804091aa7666738aa8cf6df6a203839
xiaoyangmie <juyuanyang@gmail.com>
2012-11-09 13:18:28 -0500

    slightly change the command

--- ml4profiler/new.pl
+++ ml4profiler/new.pl
@@ -8,15 +8,17 @@ use Cwd 'abs_path';
 
 my $cur_dir = getcwd;
 my $num_runs = 5;
-my $max_terms = 3;
+my $max_terms = 9;
+#my $output_dir = "/home/yjy/workspace_profiler/output/m0";
+my $output_dir = "output";
 
     print "*** Building performance model\n";
 
-    my $cmd = "octave -qf -p $cur_dir/ml/common $cur_dir/ml/stable/foba_poly_model_init.m output 1 $max_terms";
+    my $cmd = "octave -qf -p $cur_dir/ml/common $cur_dir/ml/stable/foba_poly_model_init.m $output_dir 1 $max_terms";
     print "Running command: $cmd ...\n";
     system($cmd) == 0 or die "Failed: $cmd: $!\n";
 
-    my $cmd = "octave -qf -p $cur_dir/ml/common $cur_dir/ml/stable/foba_poly_model.m output 2 $max_terms";
+    my $cmd = "octave -qf -p $cur_dir/ml/common $cur_dir/ml/stable/foba_poly_model.m $output_dir 2 $max_terms";
     print "Running command: $cmd ...\n";
     system($cmd) == 0 or die "Failed: $cmd: $!\n";
 
