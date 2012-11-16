be10cfe7f5efa7ebd7c017a695a38f6f2fcc811a
xiaoyangmie <juyuanyang@gmail.com>
2012-11-16 13:01:28 -0500

    clean up some unnecessary bin or class folder

--- apprepo/FaceDetector_new/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/vincent/Workspace/gitrepo/Android-App-Profiling/apprepo/FaceDetector_new/bin/AndroidManifest.xml : \
-/home/vincent/Workspace/gitrepo/Android-App-Profiling/apprepo/FaceDetector_new/AndroidManifest.xml \
