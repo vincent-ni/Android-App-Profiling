4dc615839f67da092c3d9e3100f4bed2a1fb9b69
xiaoyangmie <juyuanyang@gmail.com>
2012-12-08 23:10:34 -0500

    update the test formula function, fix a bug

--- app_inst/src/edu/gatech/log/helper/testFormula.java
+++ app_inst/src/edu/gatech/log/helper/testFormula.java
@@ -4,40 +4,63 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class testFormula {  
 	private static String folder = "/home/yjy/workspace_profiler/ml/";   	
-	private static String resultPath = "/home/yjy/workspace_profiler/output/m1/";
+	private static String resultPath = "/home/yjy/experiment/data_file/chess_tablet/m0/";
 //	private static String folder = "/home/vincent/Workspace/gitrepo/Android-App-Profiling/ml4profiler/"; 
 //	private static String resultPath = "/home/vincent/Workspace/output/m0/";
 	private static String resultFile = "currently_chosen_features.txt";
 	private static String featDataFile = "feature_data.txt";
+	private static String execConstFile = "exectime_const.txt";
 	private static String execTimeFile = "exectime.txt";
-	private static int expTimes = 1;
+	private static int expTimes = 10;
 	private static int maxTerms = 5;
 	private static List<Integer> featSelected = new ArrayList<Integer>();
 	private static int[][] featData;
 	private static double[][] normalizedData;
 	private static double[] execTime;
 	private static String formula;
+	private static List<String> outputStreamList = new ArrayList<String>();
+	private static List<String> randomStreamList = new ArrayList<String>();
+	private static List<Integer> randomArrayList = new ArrayList<Integer>();
+	private static int num_train = 0;
+	private static int exect_const = 0; 
 	
 	public static void main(String[] args) {
+		readFeatureData();
+		readExectime();
+		readExectConst();
 		runPerls();
+		
+//		testOneFormula("M(f) =    0.232 + 1.527*f1 + 7.260*f1*f3 + 0.901*f3");
+		dealWithRandomStream();
+		testOneFormula(formula);
+//		testOneFormula("M(f) =    0.241 + 2.082*f1 + 6.777*f1*f3 + 0.631*f3");
+		printOutputStream();
+	}
+	
+	private static void testOneFormula(String formula){
 		formula = formula.replace(" ", "");
 		formula = formula.substring(formula.indexOf("=") + 1);
 		
-		readFeatureData();
-		readExectime();
+		System.out.println(formula);
+		
 		double totalRatio = 0;
+//		for(int i = num_train; i < randomArrayList.size(); i++){
+//			totalRatio += evaluateFormula(randomArrayList.get(i) - 1, formula);
+//		}
+//		System.out.println(totalRatio / (randomArrayList.size() - num_train));
 		for(int i = 0; i < execTime.length; i++){
-			totalRatio += evaluateFormula(i);
+			totalRatio += evaluateFormula(i, formula);
 		}
 		System.out.println(totalRatio / execTime.length);
 	}
 	
-	private static double evaluateFormula(int run){
+	private static double evaluateFormula(int run, String formula){
 		int startPos = 0, endPos = -1;
 		boolean plusOp = true;
 		double result = 0;
@@ -63,8 +86,13 @@ public class testFormula {
 			result -= calExp(formula.substring(startPos, endPos), run);
 		}
 		
-		System.out.println("Actual: " + execTime[run] + ", estimated: " + result 
-				+ ", ratio:" + result/execTime[run]);
+//		System.out.println("Actual: " + execTime[run] + ", estimated: " + result 
+//				+ ", ratio:" + result/execTime[run]);
+		DecimalFormat df = new DecimalFormat("#.##");
+		DecimalFormat df1 = new DecimalFormat("#.####");
+		System.out.println(df.format(execTime[run]) + "\t" + df.format(result) + "\t" 
+					+ (int)(execTime[run]*exect_const) + "\t" + (int)(result*exect_const) + "\t" 
+					+ df1.format(result/execTime[run]));
 		return result/execTime[run];
 	}
 	
@@ -139,11 +167,24 @@ public class testFormula {
 				for(int j = 0; j < runNum; j++){
 					normalizedData[i][j] = (featData[i][j] - min) / range;
 				}
+//				System.out.println(min + " " + range);
 			}
 			
 			
 		} catch(Exception e){
 			e.printStackTrace();
+		}
+	}
+	
+	private static void readExectConst(){
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(new File(resultPath + execConstFile)));
+			String string = reader.readLine();
+			exect_const = Integer.parseInt(string.trim());
+			reader.close();
+			System.out.println("exect_const: " + exect_const);
+		} catch(Exception e){
+			
 		}
 	}
 	
@@ -181,6 +222,18 @@ public class testFormula {
 					System.out.println("Fatal error");
 					return;
 				}
+				BufferedReader firstReader = new BufferedReader(new InputStreamReader(
+						process.getInputStream()));
+				List<String> tempStrList = new ArrayList<String>();
+				String firstStr = firstReader.readLine();
+				while(firstStr != null){
+//					System.out.println("1: " + firstStr);
+					tempStrList.add(firstStr);
+					firstStr = firstReader.readLine();
+				}
+				firstReader.close();
+//				System.out.println("3");
+				
 				process = run.exec("octave -qf -p " + folder + "ml/common " + folder
 						+ "ml/stable/foba_poly_model.m " + resultPath + " 2 " + maxTerms);
 				exitValue = process.waitFor();
@@ -194,12 +247,13 @@ public class testFormula {
 				String str = reader.readLine();
 				reader.close();
 				String[] strs = str.split(" ");
-				Double error = (double) 0;
+				Double error = (double) 100;
 				if(!strs[0].contains("Inf"))
 					error = Double.parseDouble(strs[0]);
 				if(error < miniError){
 					miniError = error;
 					featSelected.clear();
+					outputStreamList.clear();
 					for(int i = 1; i < strs.length; i++)
 						featSelected.add(Integer.parseInt(strs[i]));
 					BufferedReader procReader = new BufferedReader(new InputStreamReader(
@@ -207,14 +261,32 @@ public class testFormula {
 					
 					String string = procReader.readLine();
 					while(string != null){
+//						System.out.println("2: " + string);
+						outputStreamList.add(string);
 						if(string.contains("M(f)"))
 							formula = string;
 //						System.out.println(string);
 						string = procReader.readLine();
 					}
+					randomStreamList.clear();
+					randomStreamList.addAll(tempStrList);
+//					testOneFormula(formula);
 				}
+//				if(error < 0.1){
+//					BufferedReader procReader = new BufferedReader(new InputStreamReader(
+//							process.getInputStream()));
+//
+//					String string = procReader.readLine();
+//					while(string != null){
+//						if(string.contains("M(f)")){
+////							System.out.println(error + " " + string);
+////							testOneFormula(string);
+//						}
+//						string = procReader.readLine();
+//					}
+//				}
 			}
-			System.out.println(miniError);
+			System.out.println("error: " + miniError);
 			for(int i = 0; i < featSelected.size(); i++){
 				System.out.print(featSelected.get(i) + " ");
 			}
@@ -225,4 +297,39 @@ public class testFormula {
 		}
 	}
 
+	public static void printOutputStream(){
+		for(int i = 0; i < randomStreamList.size(); i++){
+			System.out.println(randomStreamList.get(i));
+		}
+		for(int i = 0; i < outputStreamList.size(); i++){
+			System.out.println(outputStreamList.get(i));
+		}
+	}
+	
+	private static void dealWithRandomStream(){
+		for(int i = 0; i < randomStreamList.size(); i++){
+			if(randomStreamList.get(i).contains("Columns")){
+				String toAnalyze = randomStreamList.get(i + 2);
+				String[] strs = toAnalyze.split(" ");
+				for(String numStr : strs){
+					numStr = numStr.trim();
+					if(numStr.length() > 0){
+						int number = Integer.parseInt(numStr);
+						randomArrayList.add(number);
+					}
+				}
+//				System.out.println(strs);
+			}
+		}
+		for(int i = 0; i < outputStreamList.size(); i++){
+			if(outputStreamList.get(i).contains("num_train")){
+				String temp = outputStreamList.get(i + 1).trim();
+				num_train = Integer.parseInt(temp);
+				break;
+			}
+		}
+		System.out.println(num_train);
+		System.out.println(randomArrayList);
+		System.out.println(randomArrayList.size());
+	}
 }
